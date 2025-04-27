eaba49900967c7b6c5e117bc2171413d48c001e1
R2DWess <gusterlima@hotmail.com>
2025-04-27 02:49:20 -0300

    test(config): Created MsCatalogoApplicationTests

--- src/test/java/br/com/wzzy/mscatalogo/MsCatalogoApplicationTests.java
+++ src/test/java/br/com/wzzy/mscatalogo/MsCatalogoApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.wzzy.mscatalogo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MsCatalogoApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-	@Test
-	void contextLoads() {
-	}
+public class MsCatalogoApplicationTests {
 
+    @Test
+    void applicationFailsToStartWithInvalidArgs() {
+        String[] invalidArgs = {"--invalid.property=value"};
+        assertThrows(Exception.class, () -> MsCatalogoApplication.main(invalidArgs));
+    }
 }
